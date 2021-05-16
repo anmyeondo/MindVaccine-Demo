35c3f1d776f87eb23e00bc3c1aa196929c341cff
herjn <leejungin199@naver.com>
2021-05-16 12:31:11 +0900

    consult page connection update

--- app/src/main/java/com/example/mindvaccine_demo/ConsultingFragment.java
+++ app/src/main/java/com/example/mindvaccine_demo/ConsultingFragment.java
@@ -1,64 +1,50 @@
 package com.example.mindvaccine_demo;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link ConsultingFragment#newInstance} factory method to
+ * Use the {@link ConsultingFragment} factory method to
  * create an instance of this fragment.
  */
 public class ConsultingFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public ConsultingFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment ConsultingFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static ConsultingFragment newInstance(String param1, String param2) {
-        ConsultingFragment fragment = new ConsultingFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
+    private View view;
+    private Button btn_move;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_consulting_main, container, false);
+
+        ViewGroup rootView = (ViewGroup)inflater.inflate(R.layout.fragment_consult_main,container,false);
+        btn_move= rootView.findViewById(R.id.detail_consult);
+        btn_move.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(getActivity(),MainActivity2.class);
+                startActivityForResult(intent,100);
+
+            }
+        });
+
+        return rootView;
+
+
     }
 }
---
+++ app/src/main/java/com/example/mindvaccine_demo/MainActivity2.java
@@ -0,0 +1,14 @@
+package com.example.mindvaccine_demo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2); //상담세부페이지로 화면 전환
+    }
+}
