bfadd16e8f4a6d0a4ab1f8f0a8306016c75fa169
herjn <leejungin199@naver.com>
2021-05-16 13:31:11 +0900

    consult button edit

--- app/src/main/java/com/example/mindvaccine_demo/MainActivity2.java
+++ app/src/main/java/com/example/mindvaccine_demo/MainActivity2.java
@@ -2,7 +2,12 @@ package com.example.mindvaccine_demo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
 
 public class MainActivity2 extends AppCompatActivity {
 
@@ -10,5 +15,8 @@ public class MainActivity2 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2); //상담세부페이지로 화면 전환
+
     }
+
+
 }
